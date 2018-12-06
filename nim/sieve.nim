44e840ea9e5e653deaa93d0eca6d3354d6ca33d8
shinsuke <e30752@jp.ibm.com>
2018-12-07 05:44:54 +0900

    A few change

--- nim/sieve.nim
+++ nim/sieve.nim
@@ -2,16 +2,20 @@ import sequtils, strutils, math
 
 proc sieve (maxnum: int): seq[int] =
   var nums = newSeq[uint8](maxnum + 1)
-  
+  for i in countup(3, maxnum, 2):
+    nums[i] = 1
+    
   for i in countup(3, int(ceil(sqrt(float(maxnum)))), 2):
     if nums[i] == 0:
-      for j in countup( i * 3, maxnum, i * 2):
-        if nums[j] == 0:
-          nums[j] = 1
+      continue
+      
+    for j in countup( i * 3, maxnum, i * 2):
+      if nums[j] == 1:
+        nums[j] = 0
 
   result = @[2]
   for i in countup(3, maxnum, 2):
-    if nums[i] == 0:
+    if nums[i] == 1:
       result.add(i)
       
 let primes = sieve(readLine(stdin).parseInt)
