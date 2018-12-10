b60648b866fa66adf180d648be1e321088d7e343
shinsuke <e30752@jp.ibm.com>
2018-12-10 23:02:53 +0900

    Add type option

--- common-lisp/sieve.lisp
+++ common-lisp/sieve.lisp
@@ -4,20 +4,29 @@
 
 (in-package :sieve)
 
+(declaim (optimize (speed 3) (debug 0) (safety 0) (compilation-speed 0)))
+(declaim (ftype (function (fixnum) simple-bit-vector) gen-bit-seq))
+(declaim (ftype (function (fixnum) (values simple-vector &optional fixnum)) sieve))
+
 (defun gen-bit-seq (n)
-  (make-sequence 'simple-bit-vector (1+ n) :initial-element 1))
+  (make-sequence 'simple-bit-vector n :initial-element 1))
 
 (defun sieve (maxnum)
-  (let ((nums (gen-bit-seq maxnum)))
-    (loop for n from 3 by 2 while (<= (* n n) maxnum)
+  (let ((nums (gen-bit-seq (1+ maxnum))))
+    (loop for n fixnum from 3 by 2 while (<= (* n n) maxnum)
        when (= (sbit nums n) 1)
-       do (loop for x from (* n 3) to maxnum by (* n 2)
+       do (loop for x fixnum from (* n 3) to maxnum by (* n 2)
              do (setf (sbit nums x) 0)))
-    (cons 2
-          (loop for n from 3 to maxnum by 2
-             when (= (sbit nums n) 1) collect n))))
+    (let* ((primes-cnt (- (count 1 nums) (ceiling (/ maxnum 2)) 1))
+           (primes (make-sequence 'simple-vector primes-cnt)))
+      (setf (svref primes 0) 2)
+      (loop for n fixnum from 3 to maxnum by 2
+         with x fixnum = 1
+         when (= (sbit nums n) 1)
+         do (setf (svref primes x) n)
+           (setf x (1+ x)))
+      (values primes primes-cnt))))
 
 (defun main ()
- (let ((primes (sieve (read))))
-   (format t "~A ~A~%" (length primes) (car (last primes)))))
-
+  (multiple-value-bind (primes primes-cnt) (sieve (read))
+    (format t "~A ~A~%" primes-cnt (svref primes (1- primes-cnt)))))
