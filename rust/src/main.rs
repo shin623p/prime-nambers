01679767854a413ac70cd44ba75463c944baed0d
shin623p <shin623p@gmail.com>
2018-12-31 13:43:42 +0900

    Add test.

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -36,3 +36,45 @@ fn main() {
     let primes = sieve(s.trim().parse().unwrap());
     println!("{} {}", primes.len(), primes[primes.len() - 1]);
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn sieve_test_1() {
+        assert_eq!(vec![2], sieve(2));
+    }
+
+    #[test]
+    fn sieve_test_2() {
+        assert_eq!(vec![2, 3], sieve(3));
+    }
+
+    #[test]
+    fn sieve_test_3() {
+        assert_eq!(vec![2, 3], sieve(4));
+    }
+
+    #[test]
+    fn sieve_test_4() {
+        assert_eq!(vec![2, 3, 5, 7, 11, 13, 17, 19, 23], sieve(25));
+    }
+
+    #[test]
+    fn sieve_test_5() {
+        assert_eq!(vec![2, 3, 5, 7, 11, 13, 17, 19, 23], sieve(23));
+    }
+
+    #[test]
+    fn sieve_test_6() {
+        assert_eq!(vec![2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67,
+                        71, 73, 79, 83, 89, 97],
+                   sieve(100));
+    }
+
+    #[test]
+    fn sieve_test_7() {
+        assert_eq!(78498, sieve(1000000).len());
+    }
+}
